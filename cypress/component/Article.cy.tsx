dfe05e12bc8b10cbd8106a4293a3eed128c2c651
surajaj-personal <suraj@ailmac.local>
2024-11-24 13:52:44 +0100

    addeding percy

--- cypress/component/Article.cy.tsx
+++ cypress/component/Article.cy.tsx
@@ -4,6 +4,6 @@ import Article from '../../src/Article';
 describe('Article Component', () => {
   it('renders the Article and takes a Percy snapshot', () => {
     cy.mount(<Article />);
-    cy.percySnapshot('App Component');
+    cy.percySnapshot('Article Component');
   });
 });
